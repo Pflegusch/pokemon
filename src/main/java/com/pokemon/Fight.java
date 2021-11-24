65a80643d432d8242791632adcdb2e39fdcb4e21
pflegusch <dennis.pfleger@web.de>
2021-11-24 23:23:58 +0100

    Fight constructor

--- src/main/java/com/pokemon/Fight.java
+++ src/main/java/com/pokemon/Fight.java
@@ -7,5 +7,15 @@ public class Fight {
     public Trainer winner;
     public Trainer loser;
 
-    
+    private int id = fights;
+    private int rounds = 0;
+    private Trainer turn;
+
+    Fight(Trainer trainer1, Trainer Trainer2) {
+        this.trainers[0] = trainer1;
+        this.trainers[1] = trainer1;
+        fights++;
+    }
+
+
 }
