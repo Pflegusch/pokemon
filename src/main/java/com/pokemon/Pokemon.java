0e4f845e27e2eb0c02f2a6e1ef50d4909a0823df
pflegusch <dennis.pfleger@web.de>
2021-11-24 23:42:43 +0100

    more classes

--- src/main/java/com/pokemon/Pokemon.java
+++ src/main/java/com/pokemon/Pokemon.java
@@ -6,15 +6,15 @@ public class Pokemon {
     public int hp = 0;
     public int lv = 0;
     public String name;
-    public String[] attacks = new String[4];
-    public String[] type = new String[2];
-    public String[] weaknesses = new String[2];
+    public Attack[] attacks = new Attack[4];
+    public Type[] type = new Type[2];
+    public Type[] weaknesses = new Type[2];
 
     private int id = pokemons;
     private int current_exp = 0;
     private boolean ready = false;
 
-    Pokemon(int hp, int lv, String name, String[] attacks, String[] type, String[] weaknesses) {
+    Pokemon(int hp, int lv, String name, Attack[] attacks, Type[] type, Type[] weaknesses) {
         this.hp = hp;
         this.lv = lv;
         this.name = name;
