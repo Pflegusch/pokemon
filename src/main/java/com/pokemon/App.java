cb122d1193fae4151646a545ebd236aad8b65c22
pflegusch <dennis.pfleger@web.de>
2021-12-28 22:33:49 +0100

    work with db only

--- src/main/java/com/pokemon/App.java
+++ src/main/java/com/pokemon/App.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import com.server.rest.HelloRest;
+import com.server.rest.UserRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,6 +23,7 @@ public class App
         Database database = new Database();
 
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("mariadb");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -30,68 +32,43 @@ public class App
         database.set_entitytransaction(tx);
         database.initialize();
 
+        User.entityManager = em;
+        Pokemon.connection = database.connection;
+        Pokemon.entityManager = em;
+        Fight.database = database;
+
         new HelloRest(em);
+        new UserRest(em, tx);
         SpringApplication.run(App.class);
-        
-        // final Type[] glurakType = {Type.Fire, Type.Earth};
-        // final Type[] glurakWeaknesses = {Type.Water};
-        // Pokemon glurak = new Pokemon(
-        //     360, 
-        //     80,
-        //     125,
-        //     "Glurak"
-        // );
-        // glurak.attacks[0] = em.find(Attack.class, "Blast Burn");
-        // glurak.attacks[1] = em.find(Attack.class, "Blaze Kick");
-        // glurak.attacks[2] = em.find(Attack.class, "Earthquake");
-        // glurak.attacks[3] = em.find(Attack.class, "Fire Blast");
-        // glurak.type = glurakType;
-        // glurak.weaknesses = glurakWeaknesses;
-        // //database.insert_pokemon(glurak);
 
-        // final Type[] glumandaType = {Type.Fire, Type.Earth};
-        // final Type[] gluamandaWeaknesses = {Type.Water};
-        // Pokemon glumanda = new Pokemon(
-        //     290, 
-        //     70,
-        //     180,
-        //     "Glumanda"
-        // );
-        // glumanda.attacks[0] = em.find(Attack.class, "Fire Blast");
-        // glumanda.attacks[1] = em.find(Attack.class, "Heatwave");
-        // glumanda.attacks[2] = em.find(Attack.class, "Hyper Beam");
-        // glumanda.attacks[3] = em.find(Attack.class, "Blast Burn");
-        // glumanda.weaknesses = gluamandaWeaknesses;
-        // glumanda.type = glumandaType;
-        // //database.insert_pokemon(glumanda);
 
-        // // Trainer ash = new Trainer("Ash", 5000);
-        // // ash.pokemons[0] = glurak;
-        // // Trainer barry = new Trainer("Barry", 5000);
-        // // barry.pokemons[0] = glumanda;
+        // database.set_attacks(groudon);
+        // database.set_attacks(glumanda);
+        // database.set_attacks(glurak);
 
-        Trainer ash = em.find(Trainer.class, 1);
-        Trainer barry = em.find(Trainer.class, 2);
-        
-        Pokemon glumanda = em.find(Pokemon.class, 1);
-        Pokemon groudon = em.find(Pokemon.class, 2);
-        Pokemon glurak = em.find(Pokemon.class, 3);
+        // Fight fight = new Fight(ash, barry, 250);
+        // fight.start();
 
-        ash.pokemons[0] = groudon;
-        barry.pokemons[0] = glumanda;
+        // database.insert_fight(fight);
+        // database.update_trainers(fight);
 
-        database.set_attacks(groudon);
-        database.set_attacks(glumanda);
-        database.set_attacks(glurak);
+        User pflegusch = em.find(User.class, "Pflegusch");
+        pflegusch.set_pokemons();
 
-        Fight fight = new Fight(ash, barry, 250);
-        fight.start();
-        
-        for (Trainer trainer : fight.trainers) {
-            trainer.heal_pokemons();
+        for (Pokemon pokemon : pflegusch.pokemons) {
+            pokemon.set_attacks();
         }
 
-        database.insert_fight(fight);
-        database.update_trainers(fight);
+        User alice = em.find(User.class, "alice");
+        alice.set_pokemons();
+
+        for (Pokemon pokemon : alice.pokemons) {
+            pokemon.set_attacks();
+        }
+
+        Fight fight = new Fight(pflegusch, alice, 300);
+        fight.start();
+
+
     }
 }
